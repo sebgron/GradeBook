91cd93f9c1b2fd733a2451c72817c5f551d133a2
sebgron <128095693+sebgron@users.noreply.github.com>
2023-04-07 17:50:01 +0200

    Task 4

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GradeBook.GradeBooks
+{
+	public class RankedGradeBook : BaseGradeBook
+	{
+		public RankedGradeBook(string name) : base(name)
+		{
+			Type = Enums.GradeBookType.Ranked;
+		}
+	}
+}
+
