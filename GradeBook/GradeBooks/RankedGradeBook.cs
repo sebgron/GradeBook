a943127c198931c5c35c8f5a545cce55a44c65dc
sebgron <128095693+sebgron@users.noreply.github.com>
2023-04-07 18:12:55 +0200

    Task 5

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -8,6 +8,25 @@ namespace GradeBook.GradeBooks
 		{
 			Type = Enums.GradeBookType.Ranked;
 		}
-	}
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int studentsNumber = Students.Count;
+            if (studentsNumber < 5)
+                throw new InvalidOperationException();
+
+            int betterStudents = 0;
+            foreach (var S in Students)
+                if (averageGrade < S.AverageGrade) betterStudents++;
+
+            int result = (100 * betterStudents) / studentsNumber;
+
+            if (result < 20) return 'A';
+            if (result < 40) return 'B';
+            if (result < 60) return 'C';
+            if (result < 80) return 'D';
+            return 'F';
+        }
+    }
 }
 
